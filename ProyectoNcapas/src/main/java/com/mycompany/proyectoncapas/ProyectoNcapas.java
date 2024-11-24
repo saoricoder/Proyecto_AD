30e6eecce6164976d7265580da384df2702d6d04
Betty Rodriguez <blrodriguez3@espe.edu.ec>
2024-11-24 13:20:40 -0500

    1 creacion proy
    wuiii

---
+++ ProyectoNcapas/src/main/java/com/mycompany/proyectoncapas/ProyectoNcapas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoncapas;
+
+/**
+ *
+ * @author lizet
+ */
+public class ProyectoNcapas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
